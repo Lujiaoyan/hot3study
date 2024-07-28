ed7a90676b6557e15daa9a3d9c3a4e56e465a70b
Lujiaoyan <153267639+Lujiaoyan@users.noreply.github.com>
2024-07-28 12:09:51 +0800

    Update wangning的home.py

--- pages/wangning的home.py
+++ pages/wangning的home.py
@@ -1,31 +1,52 @@
-"""wangning的网站"""
+'''wangning的网站'''
 import streamlit as st
+from PIL import Image
 
-page=st.sidebar.radio('我的首页',['我的兴趣推荐', '我的图片处理工具', '我的智慧词典', '我的留言区'])
+page = st.sidebar.radio('我的首页',['我的兴趣推荐', '我的图片处理工具', '我的智慧词典', '我的留言区'])
 
-def page1():
-    """我的兴趣推荐"""
+def page_1():
+    '''我的兴趣推荐'''
     st.write('这是我的兴趣爱好')
-    st.image('TKX.jpg')
-    
-def page2():
-    """我的图片处理工具"""
+    st.write('[太空杀]')
+    st.image("TKX.jpg")
+    st.image("TKX2.jpg")
+    st.image("TKX3.jpg")
+
+def page_2():
+    '''我的图片处理工具'''
+    st.write(':sunglasses:图片换色小程序:sunglasses:')
+    uploaded_file = st.file_uploader('上传图片',type=['png','jpeg','jpg'])
+    if uploaded_file:
+        img = Image.open(uploaded_file)
+        st.image(img)
+        st.image(img_change(img,1,0,2))
+
+def page_3():    
+    '''我的智慧词典'''
     pass
-    
-def page3():
-    """我的智慧词典"""
+
+def page_4():
+    '''我的留言区'''
     pass
-    
-def page4():
-    """我的留言区"""
-    pass
+
+def img_change(img,rc,gc,bc):
+    '''图片处理'''
+    width,height=img.size
+    img_array = img.load()
+    for x in range(width):
+        for y in range(height):
+            r = img_array[x,y][rc]
+            g = img_array[x,y][gc]
+            b = img_array[x,y][bc]
+            img_array[x,y]=(r,g,b)
+    return img
 
 if page == '我的兴趣推荐':
-    page1()
-elif page == '我的图片处理工具':
-    page2()
-elif page == '我的图片处理工具':
-    page3()
-elif page == '我的智慧词典':
-    page4()
-    
+    page_1()
+elif page == '我的图片处理工具' :
+    page_2()
+elif page == '我的图片处理工具' :
+    page_3()
+elif page == '我的智慧词典' :
+    page_4()
+
